9806fca0fb6d75d8d9563b874990cb799535f2eb
usernameRuslan <166597110+usernameRuslan@users.noreply.github.com>
2024-05-01 18:48:52 +0300

    добавил <section class="faq">
    с помощью JS реализовал плавный поворот + и открытие ответов на вопросы в FAQ

--- app.js
+++ app.js
@@ -16,3 +16,21 @@ arrowNext.click(function () {
   photoCarousel.trigger("next.owl.carousel");
   });
 });
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  var questionHeaders = document.querySelectorAll(".faq-question");
+  
+  questionHeaders.forEach(function(header) {
+      header.addEventListener("click", function() {
+          var answer = this.nextElementSibling;
+          this.querySelector(".faq-plus-sign").classList.toggle("rotate");
+          if (answer.style.display === "block") {
+              answer.style.display = "none";
+          } else {
+              answer.style.display = "block";
+          }
+      });
+  });
+});
